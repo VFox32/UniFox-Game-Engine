a52d4948798ec18db097b9b6e97134a285271077
VFox32 <128222811+VFox32@users.noreply.github.com>
2026-02-18 20:26:37 +0100

    Buffer Abstraction

---
+++ UniFox/src/UniFox/Renderer/Buffer.cpp
@@ -0,0 +1,26 @@
+#include "ufpch.h"
+#include "Buffer.h"
+
+#include "Renderer.h"
+
+#include "UniFox/Platform/OpenGL/OpenGLBuffer.h"
+
+namespace UniFox {
+    VertexBuffer* VertexBuffer::Create(float* vertices, uint32_t size) {
+        switch (Renderer::GetAPI()) {
+            case RendererAPI::None : UF_CORE_ASSERT(false, "RendererAPI::None is not supported!");
+            case RendererAPI::OpenGL : return new OpenGLVertexBuffer(vertices, size);
+        }
+        UF_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+    
+    IndexBuffer* IndexBuffer::Create(uint32_t* indices, uint32_t size) {
+        switch (Renderer::GetAPI()) {
+            case RendererAPI::None : UF_CORE_ASSERT(false, "RendererAPI::None is not supported!");
+            case RendererAPI::OpenGL : return new OpenGLIndexBuffer(indices, size);
+        }
+        UF_CORE_ASSERT(false, "Unknown RendererAPI!");
+        return nullptr;
+    }
+}
---
+++ UniFox/src/UniFox/Renderer/Renderer.cpp
@@ -0,0 +1,6 @@
+#include "ufpch.h"
+#include "Renderer.h"
+
+namespace UniFox {
+    RendererAPI Renderer::s_RendererAPI = RendererAPI::OpenGL;
+}
